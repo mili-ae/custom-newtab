c19a6a67dac137ea5e979d5c526697881e41cbf9
mili-ae <gyndenovaa@gmail.com>
2023-05-14 14:13:29 +0300

    Add AMPM toggle

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ function refreshTime() {
     var hoursDisplay = document.getElementById("hours");
     var minutesDisplay = document.getElementById("minutes");
     var dateDisplay = document.getElementById("date");
+    var cookie = getCookie("ampm");
+    
     var time = new Date();
-    var hour = time.getHours()
-    var min = time.getMinutes()
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var formattedAMPMTime = formatAMPM(time);
     var date = time.getDate() + " " + Intl.DateTimeFormat('en', { month: 'long' }).format(time);
 
-    if (hour < 10) { hour = "0" + hour };
-    if (min < 10) { min = "0" + min };
+    if (hours < 10) { hours = "0" + hours };
+    if (minutes < 10) { minutes = "0" + minutes };
 
-    hoursDisplay.innerHTML = hour;
-    minutesDisplay.innerHTML = min;
+    hoursDisplay.innerHTML = cookie != "true" ? hours : formattedAMPMTime[0];
+    minutesDisplay.innerHTML = cookie != "true" ? minutes : formattedAMPMTime[1] + formattedAMPMTime[2];
     dateDisplay.innerHTML = date
 }
 
@@ -91,6 +94,51 @@ function back() {
     }
 }
 
+function formatAMPM(currentTime) {
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var ampm = hours >= 12 ? " PM" : " AM";
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    
+    return [hours, minutes, ampm]
+}
+
+function toggleAMPM() {
+    if (getCookie("ampm") == null) { setCookie("ampm", "true", 10000); };
+    cookie = getCookie("ampm");
+    if (cookie == "true") { setCookie("ampm", "false", 10000); return; };
+
+    document.cookie = setCookie("ampm", "true", 10000);
+}
+
+function getCookie(cookieName) {
+    const name = cookieName + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+  
+    for (let i = 0; i < cookieArray.length; i++) {
+      let cookie = cookieArray[i];
+      while (cookie.charAt(0) === ' ') {
+        cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(name) === 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
+    }
+    return null;
+  }
+
+function setCookie(cookieName, cookieValue, daysToExpire) {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + daysToExpire);
+  
+    const cookie = `${encodeURIComponent(cookieName)}=${encodeURIComponent(cookieValue)};expires=${expirationDate.toUTCString()};path=/`;
+  
+    document.cookie = cookie;
+}
 
 
 setInterval(refreshTime, 1)
